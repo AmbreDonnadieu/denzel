194e02cbd9b7bf214990b512bcb3f566a8aba87f
Ambre <ambre_donnadieu@live.fr>
2019-03-13 14:44:24 +0100

    base du projet
    le tutoriel avec mongodb atlas a été fait et a servi de base pour le projet denzel

---
+++ app.js
@@ -0,0 +1,118 @@
+const sandbox = require('sandbox.js');
+
+
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+const CONNECTION_URL = "mongodb+srv://UserExample:miaou@example-uof3e.mongodb.net/test?retryWrites=true";
+const DATABASE_NAME = "Movies";
+
+var app = Express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+var database, collection;
+
+app.listen(9292, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("movie");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
+
+
+//curl -H "Accept: application/json" http://localhost:9292/movies/populate
+
+app.get("/movies/populate", (request, response) => {
+    collection.insertOne(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+//curl -H "Accept: application/json" http://localhost:9292/movies
+
+app.get("/movies", (request, response) => {
+    collection.insertOne(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+//curl -H "Accept: application/json" http://localhost:9292/movies/tt0477080
+
+app.get("/movies/:id", (request, response) => {
+    collection.insertOne(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+//curl -H "Accept: application/json" http://localhost:9292/movies/search?limit=5&metascore=77
+
+app.get("/movies/search", (request, response) => {
+    collection.insertOne(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+//curl -X POST -d '{"date": "2019-03-04", "review": "😍 🔥"}' -H "Content-Type: application/json" http://localhost:9292/movies/tt0328107
+
+app.post("/movies/:id", (request, response) => {
+    collection.insertOne(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
